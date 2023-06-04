fd937aa97e35c2ed627780c6d22dc9f166c72ca4
giogreg <nichtgregor@gmail.com>
2023-06-04 20:28:09 +0200

    add cloudflare adpater

--- sveltekit/src/routes/about/+page.svelte
+++ sveltekit/src/routes/about/+page.svelte
@@ -3,4 +3,4 @@
 </svelte:head>
 
 <h1 class="text-4xl text-center my-8 uppercase">About Page</h1>
-<p>Developed with SvelteKit, TailwindCSS and Pokeapi, deployed on Netlify.</p>
+<p>Developed with SvelteKit, TailwindCSS and Pokeapi, deployed on Cloudflare.</p>
